dc267bcdbdef14a32f682f3cf980b35d8bee49a9
otecpsgitpause <34405402+otecpsgitpause@users.noreply.github.com>
2018-06-26 14:10:04 -0400

    cambios
    cambios

--- crypto/cryptojs.js
+++ crypto/cryptojs.js
@@ -1,17 +1,17 @@
 var crypto = require('crypto-js');
 var hasCodeEnv = process.env.codeEncry;
-var jCode = process.env.jCode
+var jCode = process.env.jCode;
 const cryptoFunction = {
     encrypto: function(data) {
         var jData = JSON.stringify(data);
         console.log(jData);
-        var tdes = crypto.TripleDES.encrypt(jData, jCode);
-        var rbd = crypto.Rabbit.encrypt(tdes.toString(), jCode);
+        var tdes = crypto.TripleDES.encrypt(jData, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ');
+        var rbd = crypto.Rabbit.encrypt(tdes.toString(), 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ');
         return rbd.toString();
     },
     encode: function(d) {
         let k, ena, enb, hashCode;
-        hashCode = hasCodeEnv;
+        hashCode = '64ad0e99145bab8e4f952843a71971e9becfd5aed7e7719e6420c03e3f88332f';
 
         return new Promise((resolve, reject) => {
             k = crypto.enc.Base64.parse(hashCode).toString();
@@ -25,7 +25,7 @@ const cryptoFunction = {
     },
     decode: function(d) {
         let k, dna, dnb;
-        hashCode = hasCodeEnv;
+        hashCode = '64ad0e99145bab8e4f952843a71971e9becfd5aed7e7719e6420c03e3f88332f';
         return new Promise((resolve, reject) => {
             k = crypto.enc.Base64.parse(hashCode).toString();
             dnb = crypto.Rabbit.decrypt(d, k);
