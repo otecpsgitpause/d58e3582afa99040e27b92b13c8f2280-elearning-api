d9778bc70ad7fc32e5eac520c50bc710f9ccef1f
otecpsgitpause <34405402+otecpsgitpause@users.noreply.github.com>
2018-06-27 00:27:56 -0400

    add data
    add data

--- controller/elearning/clientes.controller.router.js
+++ controller/elearning/clientes.controller.router.js
@@ -555,29 +555,13 @@ secureRouter.post(rutas[0].ruta, (req, res, next) => {
         let data = req.body.data;
         let param = data.param;
 
-        let secuencia = {
-            updateTimeClase: () => {
-                return new Promise((resolve, reject) => {
-                    schemaClase.findOne({ '_id': param.clase }).then((docClase) => {
-
-                    })
-                })
-            },
-            updateTimeModulo: () => {
-                return new Promise((resolve, reject) => {
-
-                })
-            }, updateTimeCurso: () => {
-                return new Promise((resolve, reject) => {
-
-                })
-            }
-        }
-
-
+        schemaCliente.findOne({"correo":param.correo}).then((doc)=>{
+            respuesta.sendDev({ req: req, res: res, code: 200, respuesta: { doc: doc, error: 'Error inesperado' } });
+        })
+        
 
     } catch (e) {
-        respuesta.sendDev({ req: req, res: res, code: 500, respuesta: { doc: null, error: 'Error inesperado' } });
+        respuesta.sendDev({ req: req, res: res, code: 200, respuesta: { doc: null, error: 'Error inesperado' } });
     }
 }).post(rutas[9].ruta, (req, res, next) => {
     /**
